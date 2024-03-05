a090fe960046891738699bfe699583b91ad5b7da
MiquelVG <158332725+MiquelVG@users.noreply.github.com>
2024-03-05 19:29:02 +0100

    ReorderedCommit

--- project.py
+++ project.py
@@ -1,17 +1,4 @@
-import os
-
-print("You are a prisoner that has been held captive on a Spaceship for 6 months.\n Now it's your time to escape. A guard that you bribed will come at night time to open your cell.")
-print("The guard came and opened your cell, where will you go?")
-print("1-Go left")
-print("2-Go right")
-print("3-Go straight")
-option = int(input())
-#First we ask the question and let the user enter an answer.
-while option < 1 and option > 3:
-    print("Not valid option. Try again: ")
-    option = int(input())
 # If the answer is not a valid response it will keep asking the user to enter a response.
-
 #For every possible answer it has it's own path.
 if option == 3:
     print("You arrive to the maintenance room. These are your options:")
